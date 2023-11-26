9b334002cebeb760fe9ce821b2cc942dd14ddc2e
Vrukshali-26 <tvrukshali@gmail.com>
2023-11-26 18:14:09 +0530

    REST API created for goals

---
+++ MERN-project-YT/backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const { connectDb } = require("./config/db");
+
+connectDb();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
--- todo.ts.mongo/index.ts
+++ todo.ts.mongo/index.ts
@@ -1,19 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
-import authRoute from './route/auth.user';
-import todoRoute from './route/todo';
+import authRoute from "./route/auth.user";
+import todoRoute from "./route/todo";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.use('/auth', authRoute);
-app.use('/todo', todoRoute);
+app.use("/auth", authRoute);
+app.use("/todo", todoRoute);
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-})
+  console.log(`Listening on port ${PORT}`);
+});
 
-mongoose.connect('mongodb+srv://vrukshali:zIPH7VMFMUaHckFK@cluster0.nr7kwtr.mongodb.net/todo');
-
+mongoose.connect("");
